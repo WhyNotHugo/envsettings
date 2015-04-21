fa55a1a93dd53023159c4a21963361d9678e52cf
Hugo Osvaldo Barrera <hugo@barrera.io>
2015-04-21 13:19:33 -0300

    Remove reference to inexistent file.

--- setup.py
+++ setup.py
@@ -7,7 +7,6 @@ setup(
     author='Hugo Osvaldo Barrera',
     author_email='hugo@barrera.io',
     packages=['envsettings'],
-    package_data={'': ['logging.json']},
     url='https://github.com/hobarrera/envsettings',
     license='ISC',
     description="Read settings from environment variables."
